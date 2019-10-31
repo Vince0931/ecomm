5f31173b7cd82cd500ffd3d1d1d156b9536a126b
Vincent <vincent.cau@gmail.com>
2019-10-31 19:01:56 +0100

    add Trustproxies & filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,6 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'public' => env('FILESYSTEM_PUBLIC', 'public'),
 
     /*
     |--------------------------------------------------------------------------
@@ -59,6 +60,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'token' => env('AWS_SESSION_TOKEN'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
