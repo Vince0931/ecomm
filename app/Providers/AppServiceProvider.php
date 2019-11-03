dc97616056a21f903724288d14e2068baccd9f4b
Vincent <vincent.cau@gmail.com>
2019-11-03 11:54:27 +0100

    fix app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //Schema::defaultStringLength(191);
 
         // Make sure the directory for compiled views exist
         if (! is_dir(config('view.compiled'))) {
